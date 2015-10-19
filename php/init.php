cf3b24da672e59f51a7afc15d5bceacfcd7a05f6
Jayant Bhawal <jayantbh@users.noreply.github.com>
2015-10-19 15:01:37 +0530

    Update init.php

--- php/init.php
+++ php/init.php
@@ -1,5 +1,5 @@
 <?php
-	$con=mysqli_connect("localhost","jayantbh_flat","cyberom1","flatmate");
+	$con=mysqli_connect("HOST_NAME","USER_NAME","PASSWORD","DB_NAME");
 	if (mysqli_connect_errno()) {
 		die("<script>alert(\"Database connection failed. Reload, try again later or inform jb@jayantbhawal.in.\");</script>");
 	}
